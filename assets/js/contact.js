596707a97d0c89a62a5138c43a61d70650875f17
Emma Paris <ritushiragannavar1999@gmail.com>
2022-12-12 12:24:35 +0000

    modified thankyou msg code contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,19 +1,20 @@
 
 
-    // let button=document.getElementsByClassName('send');
+    let buttonEl = document.getElementById('submit_button').required;
 
-    // button.addEventListener('click', function() {
-    //     // let msg=document.getElementById('Thankyou_message')[0];
-    //     // msg.style.display='block';
+    buttonEl.addEventListener('click', function() {
+        let msg=document.getElementsByClassName('Thankyou_message')[0];
+        msg.style.display='block';
     //    alert('Thankyou');
-    // });
+        
+    });
     
- function required()
-    {
-        var empt = document.forms["form1"]["txt"].value;
-        if (empt !=null)
-        {
-        alert('THANK YOU!');
-        return true; 
-        }
-    }
+//  function required()
+//     {
+//         var empt = document.forms["form1"]["txt"].value;
+//         if (empt !=null)
+//         {
+//         alert('THANK YOU!');
+//         return true; 
+//         }
+//     }
