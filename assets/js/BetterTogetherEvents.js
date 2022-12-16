e3b43663db7729050d54f358dee44e079a1544d3
Emma Paris <ritushiragannavar1999@gmail.com>
2022-12-16 16:57:23 +0000

    added event js file

---
+++ assets/js/BetterTogetherEvents.js
@@ -0,0 +1,25 @@
+  var expanded=false;
+        function showList(){
+           
+            var list=document.getElementById('checkboxes');
+            if(!expanded){
+                list.style.display="block";
+                expanded=true;
+            }
+            else{
+                list.style.display="none";
+                expanded=false;
+            }
+        }
+
+       
+
+        function checkedFunction(){
+        
+        var checkedInputs = document.querySelectorAll("input:checked");
+        var doc=document.getElementsByClassName('check-number')[0];
+        doc.innerText=checkedInputs.length;
+        
+        }
+
+  
