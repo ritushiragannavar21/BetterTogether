be919137d1f5ace356410b540ca65d459dfea820
Emma Paris <ritushiragannavar1999@gmail.com>
2022-12-19 13:36:18 +0000

    js file changed

--- assets/js/BetterTogetherEvents.js
+++ assets/js/BetterTogetherEvents.js
@@ -1,25 +1,27 @@
-  var expanded=false;
-        function showList(){
-           
-            var list=document.getElementById('checkboxes');
-            if(!expanded){
-                list.style.display="block";
-                expanded=true;
-            }
-            else{
-                list.style.display="none";
-                expanded=false;
-            }
+ 
+//  window.addEventListener('mouseup',function(event){
+//     var box=document.getElementById('checkboxes');
+//     if(event.target !=box && event.target.parentNode !=box){
+//         box.style.display="none";
+//     }
+// });
+
+var expanded=false;
+    function showList(){
+        var list=document.getElementById('checkboxes');
+        if(!expanded){
+            list.style.display="block";
+            expanded=true;
         }
-
-       
-
-        function checkedFunction(){
-        
-        var checkedInputs = document.querySelectorAll("input:checked");
-        var doc=document.getElementsByClassName('check-number')[0];
-        doc.innerText=checkedInputs.length;
-        
+        else{
+            list.style.display="none";
+            expanded=false;
         }
+    }  
 
-  
+function checkedFunction(){
+     var checkedInputs = document.querySelectorAll("input:checked");
+    var doc=document.getElementsByClassName('check-number')[0];
+    doc.innerText=checkedInputs.length;
+}
+
